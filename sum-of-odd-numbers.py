fa70e25354b2f173b2975399c2e2d1454590226f
sazzarano <sam.azzarano@gmail.com>
2019-10-03 21:40:30 -0400

    row sum odds for python

---
+++ sum-of-odd-numbers.py
@@ -0,0 +1,8 @@
+def row_sum_odd_numbers(n):
+    return n ** 3
+
+print(row_sum_odd_numbers(1))
+print(row_sum_odd_numbers(2))
+print(row_sum_odd_numbers(13))
+print(row_sum_odd_numbers(19))
+print(row_sum_odd_numbers(41))
